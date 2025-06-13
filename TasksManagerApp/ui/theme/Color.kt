c68e3adac319e9a8e011603d968939dd87153be0
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2025-06-12 23:39:07 -0300

    Add files via upload

---
+++ TasksManagerApp/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.TasksManagerApp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// custom colors
+val Green80 = Color(0xFF9DF591)
+val GreenGrey80 = Color(0xFFB9DBBA)
+val DeepGreen= Color(0xFF5F8A5C)
+
+val Green40 = Color(0xFF5F8A5C)
+val GreenGrey50 = Color(0xFF323B32)
+val LightGreen = Color(0xFFCAFCAE)
